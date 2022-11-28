fe5a69b9346051ad36bc935300ea52d3a7108663
husam-6 <husam@almanakly.com>
2022-11-27 22:59:57 -0500

    Make constants uppercase

--- neural_network/generate.py
+++ neural_network/generate.py
@@ -10,14 +10,14 @@ from sklearn.model_selection import train_test_split
 
 
 # %% Read and parse input files
-train_file = "./samples/SAheart.txt"
-test_file = "./samples/SAheart_test.txt"
-init_file = "./samples/SAheart_init.txt"
+TRAIN_FILE = "./samples/SAheart.txt"
+TEST_FILE = "./samples/SAheart_test.txt"
+INIT_FILE = "./samples/SAheart_init.txt"
 
 df = pd.read_csv("./samples/SAheart.csv").drop(["row.names", "famhist"], axis=1)
 train, test = train_test_split(df, test_size=0.2, random_state=31415)
-np.savetxt(train_file, train.values, fmt='%0.3f')
-np.savetxt(test_file, test.values, fmt='%0.3f')
+np.savetxt(TRAIN_FILE, train.values, fmt='%0.3f')
+np.savetxt(TEST_FILE, test.values, fmt='%0.3f')
 
 
 # %% Add initial line with number of examples, number if inputs, and number of outputs
@@ -25,12 +25,12 @@ num_inputs = df.columns.values.shape[0] - 1
 num_outputs = 1
 num_hidden = 41
 
-with open(train_file, 'r+') as f:
+with open(TRAIN_FILE, 'r+') as f:
     content = f.read()
     f.seek(0, 0)
     f.write(f"{train.shape[0]} {num_inputs} {num_outputs}\n{content}")
 
-with open(test_file, 'r+') as f:
+with open(TEST_FILE, 'r+') as f:
     content = f.read()
     f.seek(0, 0)
     f.write(f"{test.shape[0]} {num_inputs} {num_outputs}\n{content}")
@@ -49,14 +49,14 @@ def xavier_init(input_dim, output_dim, rng):
 
 np_rng = np.random.default_rng(31415)
 w = xavier_init(num_inputs + 1, num_hidden, np_rng)
-np.savetxt(init_file, w.T, fmt='%0.3f')
+np.savetxt(INIT_FILE, w.T, fmt='%0.3f')
 output = xavier_init(num_hidden + 1, num_outputs, np_rng)
 
-with open(init_file, 'r+') as f:
+with open(INIT_FILE, 'r+') as f:
     content = f.read()
     f.seek(0, 0)
     f.write(f"{num_inputs} {num_hidden} {num_outputs}\n{content}")
 
-with open(init_file, 'a') as f:
+with open(INIT_FILE, 'a') as f:
     np.savetxt(f, output.T, fmt='%0.3f')   
 
